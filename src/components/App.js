e653a5e83b64b4727057ba64a872cd18549f547a
abhikmr333 <abhi363642@gmail.com>
2024-06-17 11:37:00 +0530

    Created Start component and rendered it on page

---
+++ src/components/App.js
@@ -0,0 +1,7 @@
+import React, { useEffect, useState } from "react";
+import Questions from "./Questions.js";
+import Start from "./Start.js";
+
+export default function App() {
+    return <Start />;
+}
---
+++ src/components/Start.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+//start screen
+export default function Start() {
+    return (
+        <main className="start">
+            <h1 className="start--title">Quizzical</h1>
+            <p className="start--description">
+                Press Start Quiz to start the quiz!
+            </p>
+            <button className="start-button">Start quiz</button>
+        </main>
+    );
+}
