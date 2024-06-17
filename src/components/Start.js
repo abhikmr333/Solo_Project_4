accee1c941019473329963039b2730db7edd8833
abhikmr333 <abhi363642@gmail.com>
2024-06-17 12:20:36 +0530

    removed Start-container div from Start component as it was not needed

--- src/components/Start.js
+++ src/components/Start.js
@@ -3,14 +3,12 @@ import React from "react";
 //start screen
 export default function Start() {
     return (
-        <section className="start-container">
-            <main className="start">
-                <h1 className="start--title">Quizzical</h1>
-                <p className="start--description">
-                    Press Start Quiz to start the quiz!
-                </p>
-                <button className="start--button">Start quiz</button>
-            </main>
-        </section>
+        <main className="start">
+            <h1 className="start--title">Quizzical</h1>
+            <p className="start--description">
+                Press Start Quiz to start the quiz!
+            </p>
+            <button className="start--button">Start quiz</button>
+        </main>
     );
 }
