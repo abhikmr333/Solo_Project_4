067382570744980c67d3278a368cde05c3d627b7
abhikmr333 <abhi363642@gmail.com>
2024-06-17 12:14:55 +0530

    Some style changes in Start component

--- src/components/Start.js
+++ src/components/Start.js
@@ -3,12 +3,14 @@ import React from "react";
 //start screen
 export default function Start() {
     return (
-        <main className="start">
-            <h1 className="start--title">Quizzical</h1>
-            <p className="start--description">
-                Press Start Quiz to start the quiz!
-            </p>
-            <button className="start--button">Start quiz</button>
-        </main>
+        <section className="start-container">
+            <main className="start">
+                <h1 className="start--title">Quizzical</h1>
+                <p className="start--description">
+                    Press Start Quiz to start the quiz!
+                </p>
+                <button className="start--button">Start quiz</button>
+            </main>
+        </section>
     );
 }
